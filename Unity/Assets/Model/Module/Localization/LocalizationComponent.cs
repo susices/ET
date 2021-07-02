608384ef89ced575bc8d81c4f75d442dbf91fbb7
susices <susices@hotmail.com>
2021-07-02 18:54:05 +0800

    更新 本地化组件

---
+++ Unity/Assets/Model/Module/Localization/LocalizationComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace ET
+{
+    /// <summary>
+    /// 本地化组件
+    /// </summary>
+    public class LocalizationComponent : Entity
+    {
+        public static LocalizationComponent Instance;
+        public SystemLanguage Language;
+    }
+    
+}
---
+++ Unity/Assets/Model/Module/Localization/LocalizationComponentSystem.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+namespace ET
+{
+    public class LocalizationComponentAwakeSystem : AwakeSystem<LocalizationComponent>
+    {
+        public override void Awake(LocalizationComponent self)
+        {
+            LocalizationComponent.Instance = self;
+            self.Language = Application.systemLanguage;
+        }
+    }
+
+    /// <summary>
+    /// 本地化组件系统
+    /// </summary>
+    public static class LocalizationComponentSystem
+    {
+        public static void SetLanguage(this LocalizationComponent self, SystemLanguage language)
+        {
+            self.Language = language;
+        }
+        
+        public static string GetText(this LocalizationComponent self, int localizationTextId)
+        {
+            switch (self.Language)
+            {
+                case SystemLanguage.Chinese:
+                case SystemLanguage.ChineseSimplified:
+                    return LocalizationTextCategory.Instance.Get(localizationTextId).CN;
+                case SystemLanguage.English:
+                    return LocalizationTextCategory.Instance.Get(localizationTextId).EN;
+                default:
+                    return null;
+            }
+        }
+
+        /// <summary>
+        /// 通过Id获取文本扩展方法
+        /// </summary>
+        /// <param name="localizationId"></param>
+        /// <returns></returns>
+        public static string LocalizedText(this int localizationId)
+        {
+            return LocalizationComponent.Instance.GetText(localizationId);
+        }
+        
+        
+        
+    }
+
+}
