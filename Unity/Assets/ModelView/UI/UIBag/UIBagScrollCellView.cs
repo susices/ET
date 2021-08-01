953bfc4701d5d078e8e0836cde5dceab518c4941
wenchao <susices@hotmail.com>
2021-08-02 00:46:02 +0800

    耿欣背包组件

---
+++ Unity/Assets/ModelView/UI/UIBag/UIBagScrollCellView.cs
@@ -0,0 +1,18 @@
+﻿using EnhancedUI.EnhancedScroller;
+using UnityEngine.UI;
+
+namespace ET
+{
+    public class UIBagScrollCellView : EnhancedScrollerCellView
+    {
+        public Text m_bagItemId;
+
+        public Text m_bagItemValue;
+
+        public void SetData(BagItem bagItem)
+        {
+            this.m_bagItemId.text = bagItem.DataId.ToString();
+            this.m_bagItemValue.text = bagItem.DataValue.ToString();
+        }
+    }
+}
